15e7b18f2618886b9e8c669fcfa8ff122ff1f033
Barnaby Shearer <b@zi.is>
2021-11-15 17:40:59 +0000

    Add docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Sphinx config."""
+
+extensions = ["sphinxcontrib.autoprogram", "sphinx.ext.autodoc"]
+
+project = "RFC 7489 & 8460 SMTP Report Monitoring Utilities."
+copyright = "2021, Barnaby Shearer"
+author = "Barnaby Shearer"
+
+master_doc = "index"
